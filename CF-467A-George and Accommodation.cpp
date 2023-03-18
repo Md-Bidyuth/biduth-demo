884e4e7311502739b23cc411c0829775c4f101bc
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-19 00:16:27 +0600

    Update CF-467A-George and Accommodation.cpp

--- CF-467A-George and Accommodation.cpp
+++ CF-467A-George and Accommodation.cpp
@@ -1,1 +1,28 @@
 
+#include<iostream>
+#include<bits/stdc++.h>
+ using namespace std;
+ 
+int main(){
+ 
+	#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+	#endif
+  
+int n;
+cin>>n;
+int p,q;int ct=0;
+while(n--){
+    cin>>p>>q;
+    if(p+2<=q)
+        ct++;
+ 
+}
+cout<<ct;
+ 
+ 
+ 
+ 
+return 0;
+  }
