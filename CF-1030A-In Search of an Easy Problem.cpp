9ed93cc8c71045ae66ed258bdfba54795cc4d439
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-19 00:18:43 +0600

    Update CF-1030A-In Search of an Easy Problem.cpp

--- CF-1030A-In Search of an Easy Problem.cpp
+++ CF-1030A-In Search of an Easy Problem.cpp
@@ -1,1 +1,20 @@
 
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int ct=0,num;
+for(int i=0; i<n; i++)
+{
+    cin>>num;
+    if(num==1)
+    ct=1;
+ 
+}
+if(ct==1)
+    cout<<"hard";
+else cout<<"easy";
+return 0;
+}
