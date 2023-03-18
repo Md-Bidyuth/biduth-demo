7549fb645a7e6197b83c34e7d349a753c6cc6d2a
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-19 00:17:34 +0600

    Update CF-200B-Drinks.cpp

--- CF-200B-Drinks.cpp
+++ CF-200B-Drinks.cpp
@@ -1,1 +1,17 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int p;
+    int sum=0;
+    for(int i=0; i<n; i++)
+    {
+        cin>>p;
+        sum=sum+p;
+    }
+    cout<<(double)sum/n;
+    return 0;
+}
