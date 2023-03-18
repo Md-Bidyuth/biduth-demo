8fe00a97a8c932c6dc7b88421a5629bdcb42aec5
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-19 00:55:19 +0600

    Add files via upload

---
+++ CF-1742A-Sum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+     cin>>t;
+     while(t--){
+     int a,b,c;
+     cin>>a>>b>>c;
+     if(a+b==c||b+c==a||a+c==b)
+     cout<<"yes"<<endl;
+     else cout<<"no"<<endl;
+
+     }
+
+    return 0;
+}
