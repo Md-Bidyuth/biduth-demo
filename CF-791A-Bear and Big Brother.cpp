2d5c5da96d28f4e37ff8d81b337e2e59041912f2
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-18 23:12:14 +0600

    Create CF-791A-Bear and Big Brother.cpp

---
+++ CF-791A-Bear and Big Brother.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include <cstring>
+ using namespace std;
+ 
+int main(){
+ 
+	#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+	#endif
+        int a,b;
+         cin>>a>>b;
+            int i;
+         for(i=0; i<100; i++){
+            a=a*3;
+            b=b*2;
+            if(a>b){
+                break;
+            }
+ 
+ 
+         }
+         cout<<i+1;
+       
+	return 0;
+} 
