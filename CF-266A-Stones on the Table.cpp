2a8508dbdc0fd4b2db58571d038b88888db52403
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-18 23:08:26 +0600

    Update CF-266A-Stones on the Table.cpp

--- CF-266A-Stones on the Table.cpp
+++ CF-266A-Stones on the Table.cpp
@@ -1,1 +1,18 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;int ct=0;
+     cin>>n;
+     string s;
+     cin>>s;
+     for(int i=0; i<n; i++)
+     {  
+         if(s[i]==s[i+1])
+            ct++;
+ 
+ 
+     }
+    cout<<ct;
+ 
+return 0;
+}
